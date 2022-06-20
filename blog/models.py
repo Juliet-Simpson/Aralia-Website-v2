7e955a36ba40761a28135f876e0b6936f631a07e
Juliet-Simpson <simpsonjulietc@gmail.com>
2022-06-20 21:44:16 +0000

    Change image field, remove upload_to

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now = True)
-    image = models.ImageField(upload_to='static/images/blog_images', null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     image_alt = models.CharField(max_length=30, blank='True')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
