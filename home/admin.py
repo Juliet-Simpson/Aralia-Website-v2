702d7a5f9072ca9471629f5eb37dc35c15173cb5
Juliet-Simpson <simpsonjulietc@gmail.com>
2022-08-14 17:16:43 +0000

    Created investor update model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import InvestorUpdate
 
-# Register your models here.
+
+class InvestorUpdateAdmin(SummernoteModelAdmin):
+    """docstring"""
+    
+    list_display = ('title', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+
+    summernote_fields = ('content',)
+
+
+admin.site.register(InvestorUpdate, InvestorUpdateAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class InvestorUpdate(models.Model):
+
+    """Investor Update database fields"""
+
+    title = models.CharField(max_length=200, unique=True)
+    updated_on = models.DateTimeField(auto_now = True)
+    image = models.ImageField(upload_to='static/images/investor_images', null=True, blank=True)
+    image_alt = models.CharField(max_length=30, blank='True')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
