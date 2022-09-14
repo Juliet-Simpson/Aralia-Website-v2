353d642a5e3518eb37b4b1b9c031897b8964461d
Daniel Wright <obsidianhead@gmail.com>
2022-09-14 08:37:19 -0400

    testing serve json file

--- digitalassets/urls.py
+++ digitalassets/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('assetslink.json', views.digital_assets, name='assetslink')
+    path('/assetslink.json', views.digital_assets, name='assetslink')
 ]
