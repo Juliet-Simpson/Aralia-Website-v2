b716bbf86a33c006d6273f117c269b34282c3176
Juliet-Simpson <simpsonjulietc@gmail.com>
2022-09-27 21:14:28 +0000

    Changed contact email

--- aralia_website/settings.py
+++ aralia_website/settings.py
@@ -33,7 +33,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aralia-website-v2.herokuapp.com', 'localhost', 'https://8000-julietsimpson-araliawebs-twxra6jsyyb.ws-eu34xl.gitpod.io/', '.aralia.live']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-julietsimps-araliawebsi-p6uyosk1698.ws-eu64.gitpod.io']
+CSRF_TRUSTED_ORIGINS = ['https://8000-julietsimps-araliawebsi-p6uyosk1698.ws-eu67.gitpod.io']
 
 SECURE_CROSS_ORIGIN_OPENER_POLICY = None
 
--- contact/views.py
+++ contact/views.py
@@ -22,7 +22,7 @@ def contact(request):
         message = "\n".join(body.values())
         try:
             # Django requires the format  send_mail to be ('subject', 'message,'from_email', ['to_email'])
-            send_mail(subject, message, 'simpsonjulietc@gmail.com', ['simpsonjulietc@gmail.com'])
+            send_mail(subject, message, 'admin@araliasystems.com', ['admin@araliasystems.com'])
         except BadHeaderError:
             return HttpResponse('Invalid header found.')
         return redirect('success')
